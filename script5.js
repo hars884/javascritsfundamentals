cd78349338d8ed610a159524a70d541d5cfa43a2
hars <harshini8564@gmail.com.com>
2025-10-16 15:12:39 +0530

    completed till 10th

---
+++ script5.js
@@ -0,0 +1,11 @@
+function init(){
+    console.log("this is an initial message in console");
+    alert("this is the 5th script answer");
+}
+// for a initial changes and alert
+
+let setupEvent=()=>{
+   document.getElementById("myBtn").innerHTML="Clicked"; 
+}
+// to add a event 
+init();
